efa25402580c0e4b1753a856156588060a8ad681
VMihalov <34415280+VMihalov@users.noreply.github.com>
2020-11-25 23:34:06 +0300

    updated triangles

--- triangles/main.cpp
+++ triangles/main.cpp
@@ -1,133 +1,176 @@
-#include <iostream>
+﻿#include <iostream>
+#include <limits>
 
-using namespace std;
-
-int x = 10;
-int y = 10;
+int size = 0;
 char space = ' ';
 char symbol;
 
-void upTriangle(string t[::y][::x]) {
-    int a = 0, b = ::x;
+void upTriangle(char* arr) {
+    int a = 0, b = ::size;
 
-    for (int i = ::y/2; i < ::y; i++) {
-        for (int c = 0; c < ::x; c++) {
+    for (int i = 0; i < ::size; i++) {
+        for (int c = 0; c < ::size; c++) {
             if (c >= a && c < b) {
-                t[i][c] = symbol;
-            } else {
-                t[i][c] =  space;
+                arr[i * ::size + c] = ::symbol;
             }
         }
         a++;
         b--;
-
-        cout << endl;
     }
 
 }
 
-void downTriangle() {
-    int a = ::x/2, b = ::x/2;
+void downTriangle(char* arr) {
+    int a = ::size % 2 == 0 ? size / 2 - 1 : size / 2;
+    int b = ::size % 2 == 0 ? a + 1 : a;
 
-    for (int i = 0; i < ::y; i++) {
-        for (int c = 0; c < ::x; c++) {
-            if (c >= a && c <= b) cout << symbol;
-            else cout << space;
+
+    for (int i = 0; i < ::size; i++) {
+        for (int c = 0; c < ::size; c++) {
+            if (c >= a && c <= b && i >= ::size / 2) {
+                arr[i * ::size + c] = ::symbol;
+            }
         }
-        a--;
-        b++;
-        cout << endl;
+        if (i >= ::size / 2) {
+            a--;
+            b++;
+        }
     }
 
 }
 
-void leftTriangle() {
-    int a = ::x/2, b;
+void leftTriangle(char* arr) {
+    int a = ::size % 2 == 0 ? ::size / 2 - 1 : ::size / 2;
+    int isEven = ::size % 2 == 0 ? 1 : 0;
 
-    for (int i = 0; i <= ::y; i++) {
-        for (int c = 0; c < ::x; c++) {
-            if (c < a || c > ::x/2) cout << space;
-            else cout << symbol;
+    for (int i = 0; i <= ::size; i++) {
+        for (int c = 0; c < ::size; c++) {
+            if (c < a || c > ::size / 2 - isEven) {}
+            else arr[i * ::size + c] = symbol;
         }
 
-        if (i < ::y/2) a--;
-        else a++;
+        if (i < (::size / 2) - isEven) a--;
+        else if (i > (::size / 2) - isEven) a++;
 
-        cout << endl;
-
+        if (i == ::size / 2 && isEven == 0) a++;
     }
 
 }
 
-void rightTriangle() {
-    int a = ::x/2, b;
+void rightTriangle(char* arr) {
+    int a = (::size / 2) + 1;
+    int isEven = ::size % 2 == 0 ? 1 : 0;
 
-    for (int i = 0; i <= ::y; i++) {
-        for (int c = 0; c < ::x; c++) {
-            if (c < a && c >= ::x/2 - 1) cout << symbol;
-            else cout << space;
+    for (int i = 0; i <= ::size; i++) {
+        for (int c = 0; c < ::size; c++) {
+            if (c < a && c > ::size / 2 - 1) arr[i * ::size + c] = symbol;
         }
 
-        if (i < ::y/2) a++;
+        if (i < (::size / 2) - isEven) a++;
         else a--;
 
-        cout << endl;
+        if (i == (::size / 2) - 1 && isEven == 1) a++;
+    }
+}
+
+void insertNumber(int& var, int start, int stop) {
+    while (true) {
+        std::cout << "\n: ";
+        std::cin >> var;
+
+        if (std::cin.good()) {
+            if (var < start || var > stop) {
+                std::cout << "\nNumber must be from " << start << " to " << stop << std::endl;
+                goto error;
+            }
+
+            break;
+        }
+
+        if (std::cin.fail()) {
+            error:
+            std::cerr << "Try again!\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            
+        }
+    }
+}
+
+void insertChar(char& var) {
+    while (true) {
+        std::cout << "\n: ";
+        std::cin >> var;
+
+        if (std::cin.good()) {
+            if (std::cin.get() != 10) {
+                std::cout << "Only 1 symbol!";
+                goto error;
+            }
+
+            break;
+        }
+
+        if (std::cin.fail()) {
+            error:
+
+            std::cout << "\nTry again!";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
     }
 }
 
 int main() {
-    int width, height, orientation;
-    char space, triangle;
-    char arr[::y][::x];
-
-//    for (int y = 0; y < ::y; y++) {
-//        for (int x = 0; x < ::x; x++) {
-//            arr[y][x] = '-';
-//        }
-//    }
+    int orientation;
 
     while (true) {
+        std::cout << "Size";
+        insertNumber(::size, 1, 100);
 
-        cout << "Input width: ";
-        cin >> ::x;
+        std::cout << "\nSpace";
+        insertChar(::space);
 
-        cout << "Input height: ";
-        cin >> ::y;
+        std::cout << "\nTriangle";
+        insertChar(::symbol);
 
-        cout << "Space: ";
-        cin >> ::space;
+        std::cout << "\n1 - Up 2 - Down 3 - Left 4 - Right \nType: ";
+        insertNumber(orientation, 1, 4);
 
-        cout << "Triangle: ";
-        cin >> ::symbol;
+        char image[::size][::size];
 
-        cout << "Orientation: 1 - Up 2 - Down 3 - Left 4 - Right \n Type: ";
-        cin >> orientation;
-
+        for (int y = 0; y < ::size; y++) {
+            for (int x = 0; x < ::size; x++) {
+                image[y][x] = ::space;
+            }
+        }
 
         switch (orientation) {
-            case 1:
-                upTriangle(arr[::y][::x]);
-                break;
-            case 2:
-                downTriangle();
-                break;
-            case 3:
-                leftTriangle();
-                break;
-            case 4:
-                rightTriangle();
-                break;
+        case 1:
+            upTriangle(*image);
+            break;
+        case 2:
+            downTriangle(*image);
+            break;
+        case 3:
+            leftTriangle(*image);
+            break;
+        case 4:
+            rightTriangle(*image);
+            break;
         }
 
-        cout << "Show array: \n";
+        std::cout << "\nYour triangle: \n\n";
 
-        for (int y = 0; y < ::y; y++) {
-            for (int x = 0; x < ::x; x++) {
-                cout << arr[y][x];
+        for (int y = 0; y < ::size; y++) {
+            for (int x = 0; x < ::size; x++) {
+                std::cout << image[y][x];
             }
-            cout << endl;
+            std::cout << std::endl;
         }
 
+        std::cout << std::endl;
     }
+
     return 0;
 }
