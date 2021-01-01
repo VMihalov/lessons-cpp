fc101f62104843a8ebaa1cc7df2cdc7ce0ba5f99
VMihalov <34415280+VMihalov@users.noreply.github.com>
2021-01-01 22:30:23 +0200

    Update todo.cpp

--- todo/todo.cpp
+++ todo/todo.cpp
@@ -17,165 +17,26 @@ struct Case {
     std::string updated_at = "";
 };
 
-int size = 4;
+int size = 0;
 Case::Date currentDate;
 
-short isValidDate(short minutes, short hour, short day, short month, int year)
-{
-	if (year < currentDate.year || year >= 2100)
-        return 1;
+void createController(Case* todo);
+void updateController(Case* todo);
+void deleteController(Case* todo);
+void optionController();
 
-    if (month < 1 || month > 12)
-        return 1;
+short initializeTime(Case::Date time);
 
-    if (day < 1 || day > 31)
-        return 1;
-	
-	return 0;
-}
+short validateDate(Case::Date form);
+short validateForm(Case form);
 
-Case inputForm() {
-    Case res;
-    unsigned short day, month, year;
-    short error;
+void showTable(Case* arr, short mode = 0);
+void viewSingleCase(Case row);
 
-    std::cout << "Title: ";
-    std::cin >> res.title;
+Case inputForm();
 
-    std::cout << "Description: ";
-    std::cin.ignore();
-    std::getline(std::cin, res.description);
-
-    std::cout << "Priority: ";
-    std::cin >> res.priority;
-
-    std::cout << "Deadline:\n";
-    while(true) {
-        std::cout << "\tHour: ";
-        std::cin >> res.deadline.hour;
-        std::cout << "\tMinutes: ";
-        std::cin >> res.deadline.minutes;
-        std::cout << "\tDay: ";
-        std::cin >> res.deadline.day;
-        std::cout << "\tMonth: ";
-        std::cin >> res.deadline.month;
-        std::cout << "\tYear: ";
-        std::cin >> res.deadline.year;
-
-        error = isValidDate(res.deadline.minutes, res.deadline.hour, res.deadline.day, res.deadline.month, res.deadline.year);
-
-        if (error)
-            std::cout << "Try again!\n";
-        else
-            break;
-    }
-
-    res.created_at = "";
-    res.updated_at = "";
-
-    return res;
-}
-
-void viewSingleCase(Case row) {
-    std::cout << " |\t"
-            << row.title
-            << "\t|\t"
-            << row.description
-            << "\t|\t"
-            << row.priority
-            << "\t|\t"
-            << std::to_string(row.deadline.hour) + ':' + std::to_string(row.deadline.minutes) + ' ' + std::to_string(row.deadline.day) + '.' + std::to_string(row.deadline.month) + '.' + std::to_string(row.deadline.year)
-            << "\t|\n";
-}
-
-void sortBy(Case* arr) {
-    Case* sortArray = new Case[::size];
-    Case tmp;
-    sortArray = arr;
-
-    for (int i = 0; i < ::size; i++)
-    {
-        for (int j = 0; j < ::size; j++)
-        {
-            if (sortArray[i].priority < sortArray[j].priority) {
-                tmp = sortArray[i];
-                sortArray[i] = sortArray[j];
-                sortArray[j] = tmp;    
-            }
-        }
-        
-    }
-    
-}
-
-int create(Case* arr)
-{
-    Case req = inputForm();
-
-    arr[::size - 1] = req;
-
-    return 1;
-}
-
-int updateCase(Case* arr) {
-    int id = 0;
-    Case req;
-
-    std::cout << "Update by id: ";
-    std::cin >> id;
-
-    if (id < 0 || id >= ::size) {
-        return 0;
-    }
-
-    req = inputForm();
-
-    arr[id] = req;
-
-    return 1;
-}
-
-int deleteCase(Case* arr) {
-    int id = 0;
-    std::cout << "Delete by id: ";
-    std::cin >> id;
-
-    if (id < 0 || id >= ::size) {
-        return 0;
-    }
-
-    for (int i = id; i < ::size - 1; i++)
-    {
-        arr[i] = arr[i + 1];
-    }
-
-    return 1;
-}
-
-int searchCase(Case* arr) {
-    int type;
-    std::string value, by;
-    std::cout << "Search by: 1 - name; 2 - priority; 3 - description; 4 - deadline;\n> ";
-    std::cin >> type;
-
-    std::cout << "Insert: ";
-    std::cin.ignore();
-    std::getline(std::cin, value);
-
-    for (int i = 0; i < ::size; i++)
-    {
-        if (arr[i].title == value || arr[i].description == value) {
-            std::cout << std::endl << "| "
-                << i
-                << " |\t"
-                << arr[i].title
-                << "\t|\t"
-                << arr[i].description
-                << "\t|\n" << std::endl;
-        }
-    }
-
-    return 1;
+void errorMessage(std::string text) {
+    std::cout << "\n" + text + "\n";
 }
 
 template <typename T>
@@ -193,14 +54,242 @@ int resize(T*& arr, int expand)
     delete[] arr;
     arr = tmp;
 
+    delete[] tmp;
+
     return 1;
 }
 
-int view(Case* arr) {
-    unsigned short timestep = 1;
-    int width;
-    std::cout << "View on: 1 - today; 2 - this week; 3 - this month; 4 - all;\n> ";
-    std::cin >> timestep;
+int main()
+{
+    Case* todo = new Case[::size];
+    short action = 0;
+
+    initializeTime(currentDate);
+
+    while (true) {
+        //view(todo);
+
+        std::cout << "Menu: 0 - view; 1 - Create; 2 - Update; 3 - Delete; 4 - Search; 5 - Sort by; 6 - Settings; -1 - Exit\n> ";
+        std::cin >> action;
+
+        if (action == -1)
+            break;
+
+        switch (action)
+        {
+            case 0: {
+                showTable(todo);
+
+                break;
+            }
+            case 1:
+                createController(todo);
+                break;
+            case 2:
+                updateController(todo);
+                break;
+            case 3:
+                break;
+            case 4:
+                deleteController(todo);
+                break;
+            case 5:
+                break;
+            case 6:
+                optionController();
+                break;
+            default: 
+                std::cout << "Try again!\n";
+                break;
+        }
+
+        
+    }
+
+    delete[] todo;
+
+    return 0;
+}
+
+void createController(Case* todos) {
+    Case form = inputForm();
+
+    short validate = validateForm(form);
+
+    if (validate)
+    {
+        return errorMessage("Incorrect input data");
+    }
+
+    resize(todos, 1);
+    
+    todos[::size - 1] = form;
+    
+    return showTable(todos, 4);
+}
+
+void updateController(Case* todo) {
+    int id = 0;
+    Case req;
+
+    std::cout << "Update by id: ";
+    std::cin >> id;
+
+    if (id < 0 || id >= ::size) {
+        return errorMessage("invalid id!");
+    }
+
+    req = inputForm();
+
+    todo[id] = req;
+
+    return showTable(todo, 4);
+}
+
+void deleteController(Case* todo) {
+    int id = 0;
+    std::cout << "Delete by id: ";
+    std::cin >> id;
+
+    if (id < 0 || id >= ::size) {
+        return errorMessage("Invalid id!");
+    }
+
+    for (int i = id; i < ::size - 1; i++)
+    {
+        todo[i] = todo[i + 1];
+    }
+
+    return showTable(todo, 4);
+}
+
+void optionController() {
+    int settings = 0;
+
+    std::cout << "1 - start time; 2 - default view mod\n> ";
+    std::cin >> settings;
+
+    switch (settings)
+    {
+    case 1:
+        initializeTime(currentDate);
+        break;
+    default:
+        return errorMessage("Undefined number");
+        break;
+    }
+}
+
+short initializeTime(Case::Date time) {
+    short validate;
+
+    std::cout << "Initialize datetime\n";
+
+    do {
+        std::cout << "\tHour: ";
+        std::cin >> time.hour;
+
+        std::cout << "\tMinutes: ";
+        std::cin >> time.minutes;
+
+        std::cout << "\tDay: ";
+        std::cin >> time.day;
+
+        std::cout << "\tMonth: ";
+        std::cin >> time.month;
+
+        std::cout << "\tYear: ";
+        std::cin >> time.year;
+
+        validate =  validateDate(time);
+
+        if (validate)
+            std::cout << "Incorrect date!\nTry again!\n";
+    } while(validate);
+
+    return 0;
+}
+
+short validateDate(Case::Date form)
+{
+    if (form.hour > 60 || form.hour < 0) {
+        return 1;
+    }
+
+    if (form.minutes > 60 || form.minutes < 0) {
+        return 1;
+    }
+
+	if (form.year < 2021 || form.year >= 2100)
+        return 1;
+
+    if (form.month < 1 || form.month > 12)
+        return 1;
+
+    if (form.day < 1 || form.day > 31)
+        return 1;
+	
+	return 0;
+}
+
+short validateForm(Case form) {
+    short validate = validateDate(form.deadline);
+
+    if (validate) {
+        return 1;
+    }
+
+    return 0;
+}
+
+Case inputForm() {
+    Case res;
+    unsigned short day, month, year;
+    short error;
+
+    std::cout << "Title: ";
+    std::cin >> res.title;
+
+    std::cout << "Description: ";
+    std::cin >> res.description;
+
+    std::cout << "Priority: ";
+    std::cin >> res.priority;
+
+    std::cout << "Deadline:\n";
+    while(true) {
+        std::cout << "\tHour: ";
+        std::cin >> res.deadline.hour;
+        std::cout << "\tMinutes: ";
+        std::cin >> res.deadline.minutes;
+        std::cout << "\tDay: ";
+        std::cin >> res.deadline.day;
+        std::cout << "\tMonth: ";
+        std::cin >> res.deadline.month;
+        std::cout << "\tYear: ";
+        std::cin >> res.deadline.year;
+
+        error = validateDate(res.deadline);
+
+        if (error)
+            errorMessage("Try again!");
+        else
+            break;
+    }
+
+    res.created_at = "";
+    res.updated_at = "";
+
+    return res;
+}
+
+void showTable (Case* arr, short mode) {
+    unsigned short timestep = mode;
+    
+    if (timestep == 0) {
+        std::cout << "View on: 1 - today; 2 - this week; 3 - this month; 4 - all;\n> ";
+        std::cin >> timestep;
+    }
 
     for (int i = 0; i < ::size; i++)
     {
@@ -229,86 +318,18 @@ int view(Case* arr) {
     
 
     std::cout << std::endl;
-
-    return 1;
 }
 
-int main()
-{
-    Case* todo = new Case[::size];
-    short action = 0;
-
-    std::cout << "Initialize datetime\n\tHour: ";
-    std::cin >> currentDate.hour;
-
-    std::cout << "\tMinutes: ";
-    std::cin >> currentDate.minutes;
-
-    std::cout << "\tDay: ";
-    std::cin >> currentDate.day;
-
-    std::cout << "\tMonth: ";
-    std::cin >> currentDate.month;
-
-    std::cout << "\tYear: ";
-    std::cin >> currentDate.year;
-
-    while (true) {
-        //view(todo);
-
-        std::cout << "Menu: 0 - view; 1 - Create; 2 - Update; 3 - Delete; 4 - Search; 5 - Sort by; -1 - Exit\n> ";
-        std::cin >> action;
-
-        if (action == -1)
-            break;
-
-        switch (action)
-        {
-            case 0: {
-                view(todo);
-
-                break;
-            }
-            case 1: {
-                resize(todo, 1);
-                create(todo);
-                break;
-            }
-            case 2: {
-                int update = updateCase(todo);
-
-                if (!update) {
-                    std::cout << "\nId is invalid\n";
-                }
-                
-                break;
-            }
-            case 3: {
-                int remove = deleteCase(todo);
-
-                if (!remove) {
-                    std::cout << "\nId is invalid\n";
-                } else {
-                    resize(todo, -1);
-                }
-                
-                break;
-            }
-            case 4:
-                searchCase(todo);
-                break;
-            case 5:
-                sortBy(todo);
-                break;
-            default: 
-                std::cout << "Try again!\n";
-                break;
-        }
-
-        
-    }
-
-    delete[] todo;
-
-    return 0;
+void viewSingleCase(Case row) {
+    std::cout << " |\t"
+            << row.title
+            << "\t|\t"
+            << row.description
+            << "\t|\t"
+            << row.priority
+            << "\t|\t"
+            << std::to_string(row.deadline.hour) + ':' + std::to_string(row.deadline.minutes) + ' ' + std::to_string(row.deadline.day) + '.' + std::to_string(row.deadline.month) + '.' + std::to_string(row.deadline.year)
+            << "\t|\n";
 }
+
+
