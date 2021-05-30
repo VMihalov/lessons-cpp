83a8fb49c74cd97c3c00fec401127419ea349cea
vmihalov <vmihalov@ukr.net>
2021-05-31 00:30:40 +0300

    тема 20

--- 2 семестр/tema20-10.cpp
+++ 2 семестр/tema20-10.cpp
@@ -1,9 +1,24 @@
-#include <three.h>
+#include <iostream>
+#include "vector.h"
 
 using namespace std;
 
 int main() {
-  
+  Vector a(7, -3, -4),
+         b(3, -4, 2),
+         c(1, 0, 3);
+
+  cout << "Vectors " 
+       << "a(" << a.x << "," << a.y << "," << a.z << "), "
+       << "b(" << b.x << "," << b.y << "," << b.z << "), "
+       << "c(" << c.x << "," << c.y << "," << c.z << ")" << endl
+       << "Volume: " << parallelepipedVolume(a, b, c) << endl
+       << "Area (a and b): " << parallelogramArea(a, b) << endl
+       << "Area (b and c): " << parallelogramArea(b, c) << endl
+       << "Area (a and c): " << parallelogramArea(a, c) << endl
+       << "Angle (a and b): " << angleBetweenTwoVectors(a, b) << endl
+       << "Angle (b and c): " << angleBetweenTwoVectors(b, c) << endl
+       << "Angle (a and c): " << angleBetweenTwoVectors(a, c) << endl;
 
   return 0;
 }
---
+++ 2 семестр/vector.h
@@ -0,0 +1,38 @@
+#include <math.h>
+
+struct Vector {
+  double x, y, z;
+
+  Vector(double x, double y, double z): x(x), y(y), z(z) {} 
+};
+
+double parallelepipedVolume(Vector a, Vector b, Vector c);
+double parallelogramArea(Vector a, Vector b);
+double angleBetweenTwoVectors(Vector a, Vector b);
+double vectorModule(Vector vector);
+double scalarProductOfTwoVectors(Vector a, Vector b);
+
+double parallelepipedVolume(Vector a, Vector b, Vector c) {
+  return abs(a.x * b.y * c.z + a.y * b.z * c.x + a.z * b.x * c.y - a.z * b.y * c.x - a.x * b.z * c.y - a.y * b.x * c.z);
+}
+
+double parallelogramArea(Vector a, Vector b) {
+  Vector res(0, 0, 0);
+  res.x = a.y * b.z - a.z * b.y;
+  res.y = -1 * (a.x * b.z - a.z * b.x);
+  res.z = a.x * b.y - a.y * b.x;
+
+  return vectorModule(res);
+}
+
+double angleBetweenTwoVectors(Vector a, Vector b) {
+  return scalarProductOfTwoVectors(a, b) / (vectorModule(a) * vectorModule(b));
+}
+
+double vectorModule(Vector vector) {
+  return sqrt(pow(vector.x, 2) + pow(vector.y, 2) + pow(vector.z, 2));
+}
+
+double scalarProductOfTwoVectors(Vector a, Vector b) {
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
