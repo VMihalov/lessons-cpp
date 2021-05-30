0759659df8115836e0d4070dc1c0d9d9f77ed353
vmihalov <vmihalov@ukr.net>
2021-05-30 19:13:45 +0300

    тема 18 дороблена

--- 2 семестр/tema18-10.cpp
+++ 2 семестр/tema18-10.cpp
@@ -48,7 +48,113 @@ struct List {
   }
 
   void sort() {
+    Node* a = first;
+    Node* b = first->next;
+
+    while (a->next) {
+      while (b) {
+        if (a->product.sum > b->product.sum) {
+          swap(a, b);
+
+          a = first;
+        }
+
+        if (a->product.sum == b->product.sum) {
+          if (a->product.name > b->product.name) {
+            swap(a, b);
+
+            a = first;
+          }
+        }
+
+        b = b->next;
+      }
+
+      a = a->next;
+      b = a->next;
+    }
+  }
+
+  void swap(Node* a , Node* b) {
+    Node* aNext; Node* aPrev; Node* bNext; Node* bPrev;
+
+    if (a == first && b == last) {
+      aNext = a->next;
+      aPrev = NULL;
+
+      bNext = NULL;
+      bPrev = b->prev;
+
+      aNext->prev = b;
+      bPrev->next = a;
+
+      b->prev = NULL;
+      b->next = aNext;
+
+      a->next = NULL;
+      a->prev = bPrev;
+
+      first = b;
+      last = a;
+
+      return;
+    }
+
+    if (a->next == b && b->prev == a) {
+      a->next = b->next;
+      b->prev = a->prev;
+      a->prev = b;
+      b->next = a;
+
+      if (first == a) {
+        a->next->prev = a;
+        b->prev = NULL;
+
+        first = b;
+      } else if (last == b) {
+        a->next = NULL;
+        b->prev->next = b;
+
+        last = a;
+      } else {
+        a->next->prev = a;
+        b->prev->next = b;
+      }
+
+      a = a->prev;
+      b = a->next;
+
+      return;
+    }
+
+    aNext = a->next;
+    aPrev = a->prev;
+    bNext = b->next;
+    bPrev = b->prev;
+
+    if (a == first) {
+      bPrev->next = a;
+
+      first = b;
+    } else {
+      aPrev->next = b;
+      bPrev->next = a;
+    }
+
+    if (b == last) {
+      aNext->prev = b;
+
+      last = a;
+    } else {
+      aNext->prev = b;
+      bNext->prev = a;
+    }
+
+    b->prev = aPrev;
+    a->prev = bPrev;
     
+    b->next = aNext;
+    a->next = bNext;
   }
   
   void print() {
@@ -79,8 +185,12 @@ int main() {
 
   parseFile("products.txt", list);
 
+  cout << "From file:" << endl;
+  list.print();
+
   list.sort();
 
+  cout << endl << "Sorted products:" << endl;
   list.print();
 
   return 0;
